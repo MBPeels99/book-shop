ec9f436f734966b5332ec4f0d8e1b93fa3376abf
MBPeels99 <peelsmike3@gmail.com>
2024-01-15 12:10:07 +0100

    first commit

---
+++ DatabaseControllers.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace BookShop
+{
+    internal class DatabaseControllers
+    {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\CodingProjects\\Dot Net\\BookShop\\BookShop\\BookShop.mdf;Integrated Security=True";
+
+        public ArrayList getAllBooks()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT Title FROM books", connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    ArrayList books = new ArrayList();
+                    while (reader.Read())
+                    {
+                        books.Add(reader.GetString(0));
+                    }
+                    return books;
+                }
+            }
+        }
+
+        public ArrayList getBookInfo(string title)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT * FROM books WHERE Title = @Title";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Title", title);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        ArrayList bookInfo = new ArrayList();
+                        while (reader.Read())
+                        {
+                            bookInfo.Add(reader.GetString(0));
+                            bookInfo.Add(reader.GetString(1));
+                            bookInfo.Add(reader.GetString(2));
+                           
+                            if (reader.IsDBNull(3))
+                            {
+                                bookInfo.Add((string)null);
+                            } else
+                            {
+                                bookInfo.Add(reader.GetString(3));
+                            }
+                        }
+                        return bookInfo;
+                    }
+                }
+            }
+        }
+
+        public Boolean addNewBook(string ISBN, string Title, string Author, string Description)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO books (ISBN, Title, Author, Description) VALUES (@ISBN, @Title, @Author, @Description)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN",ISBN);
+                    command.Parameters.AddWithValue("@Title",Title);
+                    command.Parameters.AddWithValue("@Author",Author);
+                    command.Parameters.AddWithValue("@Description",Description);
+                    int rowAffected = command.ExecuteNonQuery();
+                    if (rowAffected > 0)
+                    {
+                        return true;
+                    } else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public Boolean deleteBookFromDB(string isbn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM books WHERE ISBN=@ISBN";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN",isbn);
+                    int rowAffected = command.ExecuteNonQuery();
+                    if (rowAffected > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        public Boolean updateBook(string isbn, string title, string author, string description)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE books SET Title = @Title, Author = @Author, Description = @Description WHERE ISBN = @ISBN";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN", isbn);
+                    command.Parameters.AddWithValue("@Title", title);
+                    command.Parameters.AddWithValue("@Author", author);
+                    command.Parameters.AddWithValue("@Description", description);
+                    int rowAffected = command.ExecuteNonQuery();
+                    if (rowAffected > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookShop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DatabaseControllers databaseControllers = new DatabaseControllers();
+
+        private Boolean updateToggle    = false;
+        private Boolean newBookToggle   = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            initializeBooks();
+        }
+
+        public void initializeBooks()
+        {
+            ArrayList allBooks = databaseControllers.getAllBooks();
+            StackPanel stackPanel = new StackPanel();
+
+            txtAuthor.Clear();
+            txtTitle.Clear();
+            txtISBN.Clear();
+            txtDescription.Clear();
+
+            for (int i = 0; i < allBooks.Count; i++)
+            {
+                Label label     = new Label();
+                label.Content   = allBooks[i].ToString();
+                label.FontSize  = 10;
+                label.MouseEnter += new MouseEventHandler(label_MouseEnter);
+                label.MouseLeave += new MouseEventHandler(label_MouseLeave);
+                label.MouseLeftButtonDown += new MouseButtonEventHandler(label_MouseLeftButtonDown);
+                stackPanel.Children.Add(label);
+            }
+            gbBookList.FontSize = 16;
+            gbBookList.Content = stackPanel;
+        }
+
+        private void label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Label label = sender as Label;
+            if (label != null)
+            {
+                label.FontSize = 16;
+                label.Foreground = Brushes.Red;
+            }
+        }
+
+        private void label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Label label = sender as Label;
+            if (label != null)
+            {
+                label.FontSize = 10;
+                label.Foreground = Brushes.Black;
+            }
+        }
+
+        private void label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Label label = sender as Label;
+
+            if (label != null)
+            {
+                //TODO: Get info from Database
+                string title = (string)label.Content;
+                ArrayList bookInfo = databaseControllers.getBookInfo(title);
+
+                txtISBN.Text         = bookInfo[0].ToString();
+                txtTitle.Text        = bookInfo[1].ToString();
+                txtAuthor.Text       = bookInfo[2].ToString();
+                if (bookInfo[3] == null)
+                {
+                    txtDescription.Text = "Add Description here";
+                } else
+                {
+                    txtDescription.Text = bookInfo[3].ToString();
+                }
+                
+                // Set textboxes to read-only
+                txtISBN.IsReadOnly          = true;
+                txtTitle.IsReadOnly         = true;
+                txtAuthor.IsReadOnly        = true;
+                txtDescription.IsReadOnly   = true;
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            // Set textboxes to editable
+            txtTitle.IsReadOnly        = false;
+            txtAuthor.IsReadOnly       = false;
+            txtDescription.IsReadOnly  = false;
+
+            //Set updateToggle to true, so that it can be checked in the save button
+            updateToggle = true;
+
+            //Turn off other toggle buttons
+            newBookToggle = false;
+        }
+
+        private void buttonNewBook_Click(object sender, RoutedEventArgs e)
+        {
+            txtAuthor.Clear();
+            txtTitle.Clear();
+            txtISBN.Clear();
+            txtDescription.Clear();
+
+            txtISBN.IsReadOnly        = false;
+            txtTitle.IsReadOnly       = false;
+            txtAuthor.IsReadOnly      = false;
+            txtDescription.IsReadOnly = false;
+
+            newBookToggle   = true;
+
+            updateToggle    = false;
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            string isbn = txtISBN.Text;
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+            string description = txtDescription.Text;
+
+            if (newBookToggle)
+            {
+                Boolean rowsAffected = databaseControllers.addNewBook(isbn, title, author, description);
+                if (rowsAffected)
+                {
+                    MessageBox.Show("Book as been succesfully added","Succes",MessageBoxButton.OK);
+                    initializeBooks();
+                }
+            }
+
+            if (updateToggle)
+            {
+                Boolean rowsAffected = databaseControllers.updateBook(isbn, title, author, description);
+                if (rowsAffected)
+                {
+                    MessageBox.Show("Book as been succesfully updated", "Succes", MessageBoxButton.OK);
+                    initializeBooks();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure that you want to delete this book?", "WARNING", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Boolean rowsAffected = databaseControllers.deleteBookFromDB(txtISBN.Text);
+                if (rowsAffected)
+                {
+                    MessageBox.Show("Book as been succesfully added", "Succes", MessageBoxButton.OK);
+                    initializeBooks();
+                }
+            }
+        }
+    }
+}
